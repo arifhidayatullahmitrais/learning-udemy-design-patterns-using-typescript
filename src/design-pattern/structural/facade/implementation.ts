1d585787280ac290159b0eae1216453cd38fe915
arif hidayatullah <arif.hidayatullah@mitrais.com>
2025-08-13 09:53:20 +0700

    feat(structural): facade implementation

---
+++ src/design-pattern/structural/facade/implementation.ts
@@ -0,0 +1,64 @@
+class Amplifier {
+  public turnOn(): void {
+    console.log('Amplifier is turned On')
+  }
+
+  public setVolume(level: number): void {
+    console.log(`Volume is set to ${level}`)
+  }
+}
+
+class DvdPlayer {
+  public turnOn(): void {
+    console.log('The DVD Player is tuned on')
+  }
+
+  public play(movie: string): void {
+    console.log(`Playing ${movie}`)
+  }
+}
+
+class Projector {
+  public turnOn(): void {
+    console.log('Projector is turned on')
+  }
+
+  public setInput(dvdPlayer: DvdPlayer): void {
+    console.log('Input set to DvdPlayer')
+  }
+}
+
+class Lights {
+  public dim(level: number): void {
+    console.log(`Lights dimmed to ${level}`)
+  }
+}
+
+class HomeTheaterFacade {
+  constructor(
+    private amplifier: Amplifier,
+    private dvdPlayer: DvdPlayer,
+    private projector: Projector,
+    private lights: Lights
+  ) {}
+
+  public watchMovie(movie: string, volume: number, level: number) {
+    console.log(`Get Ready To Watch ${movie}`)
+    this.lights.dim(level)
+    this.amplifier.turnOn()
+    this.amplifier.setVolume(volume)
+    this.dvdPlayer.turnOn()
+    this.projector.turnOn()
+    this.projector.setInput(this.dvdPlayer)
+    this.dvdPlayer.play(movie)
+  }
+}
+
+let amplifier = new Amplifier()
+let dvdPlayer = new DvdPlayer()
+let projector = new Projector()
+let lights = new Lights()
+
+let homeTheater = new HomeTheaterFacade(amplifier, dvdPlayer, projector, lights)
+
+homeTheater.watchMovie('Inception', 10, 5)
