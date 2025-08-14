d5306c1ee069c21ed65e12595c3ccbbc934785b5
arif hidayatullah <arif.hidayatullah@mitrais.com>
2025-08-14 22:06:02 +0700

    feat: behavioral pattern

---
+++ src/design-pattern/behavioral/strategy/implementation.ts
@@ -0,0 +1,40 @@
+interface FilterStrategy {
+  apply(image: string): void
+}
+
+class GreyScaleStrategy implements FilterStrategy {
+  public apply(image: string): void {
+    console.log(`Applying greyscale filter to ${image}`)
+  }
+}
+
+class SepiaStrategy implements FilterStrategy {
+  public apply(image: string): void {
+    console.log(`Applying sepia filter to ${image}`)
+  }
+}
+
+class NegativeStrategy implements FilterStrategy {
+  public apply(image: string): void {
+    console.log(`Applying negative filter to ${image}`)
+  }
+}
+
+class ImageProcessor {
+  constructor(private strategy: FilterStrategy) {}
+
+  public setFilterStrategy(strategy: FilterStrategy): void {
+    this.strategy = strategy
+  }
+
+  public applyFilter(image: string): void {
+    this.strategy.apply(image)
+  }
+}
+
+// Client Code
+const imageProcessor = new ImageProcessor(new GreyScaleStrategy())
+imageProcessor.applyFilter('Image.jpg')
+
+imageProcessor.setFilterStrategy(new SepiaStrategy())
+imageProcessor.applyFilter('Image2.jpg')
---
+++ src/design-pattern/behavioral/template/index.ts
@@ -0,0 +1,54 @@
+abstract class CakeRecipe {
+  public bakeCake(): void {
+    this.preHeatOven()
+    this.mixIngredients()
+    this.bake()
+    this.coolingDown()
+    this.decorate()
+  }
+
+  protected preHeatOven(): void {
+    console.log('Preheating oven to 175 Degree C')
+  }
+
+  protected bake(): void {
+    console.log('Baking cake ....')
+  }
+
+  protected coolingDown(): void {
+    console.log('Cooling down the cake ...')
+  }
+
+  protected decorate(): void {
+    console.log('Decorating cake ...')
+  }
+
+  protected abstract mixIngredients(): void
+}
+
+class ChocolateCake extends CakeRecipe {
+  protected mixIngredients(): void {
+    console.log('Mixing: chocolate, sugar, butter, flour, eggs')
+  }
+
+  protected decorate(): void {
+    console.log(' Decorating cake with chocolate')
+  }
+}
+
+class VanillaCake extends CakeRecipe {
+  protected mixIngredients(): void {
+    console.log('Mixing: vanilla extract, sugar, butter, flour, eggs')
+  }
+}
+
+// client code
+function bakecake(cake: CakeRecipe) {
+  cake.bakeCake()
+}
+
+console.log('Baking a chocolate cake')
+bakecake(new ChocolateCake())
+
+console.log('Baking a vanilla cake')
+bakecake(new VanillaCake())
